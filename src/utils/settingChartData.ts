3249b271aceafdb6c58231ea8ccda1121b8b61b5
Ankush <ankushnitjsr@gmail.com>
2024-10-16 13:33:09 +0530

    feat: Add settingChartData util function

---
+++ src/utils/settingChartData.ts
@@ -0,0 +1,53 @@
+import { ChartData } from "../types/chart-data";
+import { getDate } from "./getDate";
+
+export const settingChartData = (
+  setChartData: (data: ChartData) => void,
+  prices1: [number, number][],
+  prices2?: [number, number][]
+) => {
+  if (prices2) {
+    setChartData({
+      labels: prices1?.map((data) => getDate(data[0])),
+      datasets: [
+        {
+          label: "Crypto 1",
+          data: prices1?.map((data) => data[1]),
+          borderWidth: 1,
+          fill: false,
+          backgroundColor: "rgba(58, 128, 233,0.1)",
+          tension: 0.25,
+          borderColor: "#3a80e9",
+          pointRadius: 0,
+          yAxisID: "crypto1",
+        },
+        {
+          label: "Crypto 2",
+          data: prices2?.map((data) => data[1]),
+          borderWidth: 1,
+          fill: false,
+          tension: 0.25,
+          borderColor: "#61c96f",
+          pointRadius: 0,
+          yAxisID: "crypto2",
+        },
+      ],
+    });
+  } else {
+    setChartData({
+      labels: prices1?.map((data) => getDate(data[0])),
+      datasets: [
+        {
+          data: prices1?.map((data) => data[1]),
+          borderWidth: 1,
+          fill: true,
+          backgroundColor: "rgba(58, 128, 233,0.1)",
+          tension: 0.25,
+          borderColor: "#3a80e9",
+          pointRadius: 0,
+          yAxisID: "crypto1",
+        },
+      ],
+    });
+  }
+};
