6f412743bf030b968b362ce8f7a471aa51481ca2
Ankush <ankushnitjsr@gmail.com>
2024-10-17 17:01:40 +0530

    refactor: Remove mouse UI

--- src/App.tsx
+++ src/App.tsx
@@ -6,49 +6,8 @@ import CoinPage from "./pages/CoinPage";
 import ComparePage from "./pages/ComparePage";
 import WatchlistPage from "./pages/WatchlistPage";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useEffect } from "react";
-// import Footer from "./components/common/footer/Footer";
 
 const App = () => {
-  useEffect(() => {
-    const cursor = document.getElementById("cursor") as HTMLElement;
-    const cursorPointer = document.getElementById(
-      "cursor-pointer"
-    ) as HTMLElement;
-
-    const handleMouseMove = (e: MouseEvent) => {
-      cursor.style.left = `${e.clientX}px`;
-      cursor.style.top = `${e.clientY}px`;
-      cursorPointer.style.left = `${e.clientX}px`;
-      cursorPointer.style.top = `${e.clientY}px`;
-    };
-
-    const handleMouseDown = () => {
-      cursor.style.height = "0.5rem";
-      cursor.style.width = "0.5rem";
-      cursorPointer.style.height = "3rem";
-      cursorPointer.style.width = "3rem";
-    };
-
-    const handleMouseUp = () => {
-      cursor.style.height = "0.3rem";
-      cursor.style.width = "0.3rem";
-      cursorPointer.style.height = "2rem";
-      cursorPointer.style.width = "2rem";
-    };
-
-    document.body.addEventListener("mousemove", handleMouseMove);
-    document.body.addEventListener("mousedown", handleMouseDown);
-    document.body.addEventListener("mouseup", handleMouseUp);
-
-    return () => {
-      document.body.removeEventListener("mousemove", handleMouseMove);
-      document.body.removeEventListener("mousedown", handleMouseDown);
-      document.body.removeEventListener("mouseup", handleMouseUp);
-    };
-  }, []);
-
   return (
     <div className="App">
       <div className="cursor" id="cursor" />
