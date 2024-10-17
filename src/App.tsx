9b62d21aa6076a490d12a2f73fe92ca6011ffbc9
Ankush <ankushnitjsr@gmail.com>
2024-10-17 12:41:06 +0530

    feat: Add pages and paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,71 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashBoardPage";
+import CoinPage from "./pages/CoinPage";
+import ComparePage from "./pages/ComparePage";
+import WatchlistPage from "./pages/WatchlistPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+// import Footer from "./components/common/footer/Footer";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const cursor = document.getElementById("cursor") as HTMLElement;
+    const cursorPointer = document.getElementById(
+      "cursor-pointer"
+    ) as HTMLElement;
+
+    const handleMouseMove = (e: MouseEvent) => {
+      cursor.style.left = `${e.clientX}px`;
+      cursor.style.top = `${e.clientY}px`;
+      cursorPointer.style.left = `${e.clientX}px`;
+      cursorPointer.style.top = `${e.clientY}px`;
+    };
+
+    const handleMouseDown = () => {
+      cursor.style.height = "0.5rem";
+      cursor.style.width = "0.5rem";
+      cursorPointer.style.height = "3rem";
+      cursorPointer.style.width = "3rem";
+    };
+
+    const handleMouseUp = () => {
+      cursor.style.height = "0.3rem";
+      cursor.style.width = "0.3rem";
+      cursorPointer.style.height = "2rem";
+      cursorPointer.style.width = "2rem";
+    };
+
+    document.body.addEventListener("mousemove", handleMouseMove);
+    document.body.addEventListener("mousedown", handleMouseDown);
+    document.body.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      document.body.removeEventListener("mousemove", handleMouseMove);
+      document.body.removeEventListener("mousedown", handleMouseDown);
+      document.body.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
+
   return (
-    <>
-      <p>Ankush...</p>
-    </>
+    <div className="App">
+      <div className="cursor" id="cursor" />
+      <div className="cursor-pointer" id="cursor-pointer" />
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/coin/:id" element={<CoinPage />} />
+          <Route path="/compare" element={<ComparePage />} />
+          <Route path="/watchlist" element={<WatchlistPage />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Footer /> */}
+    </div>
   );
-}
+};
 
 export default App;
