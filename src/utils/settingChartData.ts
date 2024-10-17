f62d2ef5bd0781aed6bbd120da0da665f4deb99f
Ankush <ankushnitjsr@gmail.com>
2024-10-17 12:40:08 +0530

    refactor: Refactor settingChartData util function and Update type

--- src/utils/settingChartData.ts
+++ src/utils/settingChartData.ts
@@ -1,53 +1,50 @@
-import { ChartData } from "../types/chart-data";
+import { ChartData, dataset } from "../types/chart-data";
+import { CoinSummary } from "../types/coin-item";
 import { getDate } from "./getDate";
 
 export const settingChartData = (
   setChartData: (data: ChartData) => void,
-  prices1: [number, number][],
-  prices2?: [number, number][]
-) => {
-  if (prices2) {
+  prices1: Array<[number, number]> | undefined,
+  coin1: CoinSummary,
+  coin2?: CoinSummary,
+  prices2?: Array<[number, number]> | undefined
+): void => {
+  // Check if prices1 is defined before proceeding
+  if (!prices1) {
+    // Handle the case where prices1 is undefined
     setChartData({
-      labels: prices1?.map((data) => getDate(data[0])),
-      datasets: [
-        {
-          label: "Crypto 1",
-          data: prices1?.map((data) => data[1]),
-          borderWidth: 1,
-          fill: false,
-          backgroundColor: "rgba(58, 128, 233,0.1)",
-          tension: 0.25,
-          borderColor: "#3a80e9",
-          pointRadius: 0,
-          yAxisID: "crypto1",
-        },
-        {
-          label: "Crypto 2",
-          data: prices2?.map((data) => data[1]),
-          borderWidth: 1,
-          fill: false,
-          tension: 0.25,
-          borderColor: "#61c96f",
-          pointRadius: 0,
-          yAxisID: "crypto2",
-        },
-      ],
+      labels: [],
+      datasets: [],
     });
-  } else {
-    setChartData({
-      labels: prices1?.map((data) => getDate(data[0])),
-      datasets: [
-        {
-          data: prices1?.map((data) => data[1]),
-          borderWidth: 1,
-          fill: true,
-          backgroundColor: "rgba(58, 128, 233,0.1)",
-          tension: 0.25,
-          borderColor: "#3a80e9",
-          pointRadius: 0,
-          yAxisID: "crypto1",
-        },
-      ],
-    });
+    return; // Exit early if prices1 is not available
   }
+
+  setChartData({
+    labels: prices1.map((data) => getDate(data[0])),
+    datasets: [
+      {
+        label: coin1?.name ?? "",
+        data: prices1.map((data) => data[1]),
+        borderWidth: 1,
+        fill: true,
+        tension: 0.25,
+        backgroundColor: prices2 ? "transparent" : "rgba(58, 128, 233, 0.1)",
+        borderColor: "#3a80e9",
+        pointRadius: 0,
+      },
+      prices2
+        ? {
+            label: coin2?.name ?? "",
+            data: prices2.map((data) => data[1]),
+            borderWidth: 1,
+            fill: true,
+            tension: 0.25,
+            backgroundColor: "rgba(97, 201, 111, 0.1)",
+            borderColor: "#61c96f",
+            pointRadius: 0,
+            yAxisID: "y2",
+          }
+        : null,
+    ].filter(Boolean) as dataset[],
+  });
 };
