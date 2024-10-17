976ffd1d87309bb8e3f5cba9976c8355480e9179
Ankush <ankushnitjsr@gmail.com>
2024-10-17 12:32:51 +0530

    feat: Add Pagination component and it's style

---
+++ src/components/dashboard/pagination/PaginationController.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react";
+import Pagination from "@mui/material/Pagination";
+import "./styles.css";
+
+interface PaginationComponentProps {
+  pageNumber: number;
+  handleChange: (event: React.ChangeEvent<unknown>, value: number) => void;
+}
+
+const PaginationComponent: FC<PaginationComponentProps> = ({
+  pageNumber,
+  handleChange,
+}) => {
+  return (
+    <div className="pagination-div">
+      <Pagination
+        count={10}
+        page={pageNumber}
+        onChange={handleChange}
+        sx={{
+          color: "var(--white)",
+          "& .Mui-selected ": {
+            backgroundColor: "var(--blue) !important",
+            color: "#fff !important",
+            borderColor: "var(--blue) !important",
+          },
+          "& .MuiPaginationItem-ellipsis": {
+            border: "0px solid var(--grey) !important",
+          },
+          "& .MuiPaginationItem-text": {
+            color: "var(--white)",
+            border: "1px solid var(--grey)",
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default PaginationComponent;
